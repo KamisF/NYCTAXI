d15bff536cb3fae9ba002640f6f92e02f40166ff
KamisF <kamilafreitas10@hotmail.com>
2024-10-03 11:17:41 -0300

    Codigo

--- teste.py
+++ teste.py
@@ -1,1 +1,8 @@
-testando repo a
+import pyarrow.parquet as pq
+trips = pq.read_table('trips.parquet')
+trips = trips.to_pandas()
+
+# Ler o .parquet
+
+table2 = pq.read_table('NYCTAXI\YellowTaxiTripRecord\yellow_tripdata_2024-01.parquet')
+table2.to_pandas()
