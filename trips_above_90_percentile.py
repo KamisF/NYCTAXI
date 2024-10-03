75e81b91c40c65124a78ad081286ea45244e81c1
KamisF <kamilafreitas10@hotmail.com>
2024-10-03 14:38:23 -0300

    teste 1

--- trips_above_90_percentile.py
+++ trips_above_90_percentile.py
@@ -7,7 +7,7 @@ trips = pq.read_table('yellow_tripdata_2024-01.parquet')
 #vou me referir a tabela sendo df de dataframe
 df = trips.to_pandas()
 
-#filtrar a coluna trip distance e usar a funcao quantile por se tratar de um float
+#filtrar a coluna trip distance e usar a funcao quantile por se tratar de um float.
 percentile_90 = df['trip_distance'].quantile(0.9)
 
 #filtrar a coluna trip distance e filtrar tudo acima de 0.9.
